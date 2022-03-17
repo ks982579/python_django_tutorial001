c25d025a23f9dbfd0511828e5126b4d0c8194bf8
Kevin Sullivan <kevinsullivan@ksulldev.space>
2022-03-17 17:56:42 +0000

    Third Commit 17 Mar 2022

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,9 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#Extra Places for Collectstatic to find static files.
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -126,4 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Activate Django-Heroku
-django_heroku.setting(locals())
+django_heroku.settings(locals())
